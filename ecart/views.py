591a31f43e59cd8ffd76d608a485fa6369d96327
Sandeep-Sambyal <44477692+Sandeep-Sambyal@users.noreply.github.com>
2020-06-23 20:53:04 +0530

    code running with fetching category

--- ecart/views.py
+++ ecart/views.py
@@ -5,27 +5,23 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products =Products.objects.all()
-    print(products)
-    n = len(products)
-    nSlides = n // 4 + ceil((n / 4) - (n // 4))
-    #params = {'no_of_slides':nSlides, 'range': range(1,nSlides),'product': products}
-    allprods=[[products,range(1,nSlides),nSlides],[products,range(1,nSlides),nSlides]]
-    params={'allproducts':allprods}
-    print (params)
-
-    """allProds=[]
-           val=Products.objects.values('category','id')
-           val2={item['category'] for item in val}
-           for val2 in val2:
-               prod=Products.objects.filter(category=val2)
-               n=len(prod)
-               nSlides = n // 4 + ceil((n / 4) - (n // 4))
-               allProds.append([prod,range(1,nSlides),nSlides])
-
-
-           params={'allProds':allProds}
-           """
+    allProds = []
+    val = Products.objects.values('category', 'id')
+    val2 = {item['category'] for item in val}
+    str1 = ""
+    for val2 in val2:
+        prod = Products.objects.filter(category=val2)
+        val = [item.product_name for item in prod]
+        for a in val:
+            print(a)
+        str1 = str1 + ","
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        """str1=str1+str(prod)
+        a=[item['product_name']  in prod]
+            print(item)"""
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
     return render(request, 'ecart/index.html', params)
 
 
@@ -35,7 +31,29 @@ def about(request):
     return HttpResponse("ABOUT US PAGE")
 
 def contact(request):
-    return render(request,'ecart/contact.html')
+
+
+    allProds = []
+    val = Products.objects.values('category', 'id')
+    val2 = {item['category'] for item in val}
+    str1=""
+    for val2 in val2:
+        prod = Products.objects.filter(category=val2)
+        val=[item.product_name for item in prod]
+        for a in val:
+            print(a)
+        str1=str1+","
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        """str1=str1+str(prod)
+        a=[item['product_name']  in prod]
+            print(item)"""
+        allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds}
+        #for a in allProds:
+    #print(params)
+    return render(request,'ecart/contact.html',params)
+
 def tracker(request):
     return HttpResponse("TRACKER PAGE")
 def search(request):
