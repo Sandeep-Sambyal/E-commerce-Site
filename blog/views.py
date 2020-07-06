b1d4ae34f9173789e5d887ade3d6b92c08471860
Sandeep-Sambyal <44477692+Sandeep-Sambyal@users.noreply.github.com>
2020-07-06 22:42:59 +0530

    Blog app- blogpost page top display individual pages done.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,18 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
+from .models import blogpost
 
 # Create your views here.
 
 def index(request):
-    return render(request,'blog/index.html')
+    obj=blogpost.objects.all()
+    print(obj)
+    param={'data':obj}
+
+    return render(request,'blog/index.html',param)
+
+def blogposted(request,id):
+    post=blogpost.objects.filter(blog_id=id)[0]
+    print(post)
+    return render (request,'blog/blogpost.html',{'data':post})
